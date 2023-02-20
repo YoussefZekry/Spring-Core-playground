bf942bd3ab9231cff29744bc5729586878c607e2
youssef <youssefzekry97@gmail.com>
2023-02-20 20:51:40 +0200

    Define scope as prototype

--- src/main/java/com/mypackage/Config.java
+++ src/main/java/com/mypackage/Config.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @PropertySource("values.properties")
 public class Config {
 	
 	@Bean(initMethod = "init")
+	@Scope("prototype")
 	public Product product(){
 		return new Product();
 	}
