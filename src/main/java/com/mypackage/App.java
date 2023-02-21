1fc214bdb7e58ef8dbb9ede368e45eff5bff1b0b
youssef <youssefzekry97@gmail.com>
2023-02-21 15:58:52 +0200

    Use advisors

--- src/main/java/com/mypackage/App.java
+++ src/main/java/com/mypackage/App.java
@@ -17,8 +17,7 @@ public class App
         //retrieve bean from spring container
         CalcInterface calc = context.getBean("CalcProxy",CalcInterface.class);
         System.out.println( calc.add(5, 4));
-        System.out.println(calc.multi(5, 4)); 
-
+        System.out.println(calc.multi(5, 4));
         // context.registerShutdownHook();
     }
 }
