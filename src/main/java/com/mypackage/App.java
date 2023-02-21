662863978d27fd2d72c6482d63c5352a298f30b0
youssef <youssefzekry97@gmail.com>
2023-02-21 17:30:23 +0200

    Use autoProxy

--- src/main/java/com/mypackage/App.java
+++ src/main/java/com/mypackage/App.java
@@ -15,7 +15,7 @@ public class App
                 new ClassPathXmlApplicationContext("beans2.xml");
 
         //retrieve bean from spring container
-        CalcInterface calc = context.getBean("CalcProxy",CalcInterface.class);
+        CalcInterface calc = context.getBean("CalcService",CalcInterface.class);
         System.out.println( calc.add(5, 4));
         System.out.println(calc.multi(5, 4));
         // context.registerShutdownHook();
