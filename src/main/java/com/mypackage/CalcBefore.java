efc495be2e99d7940643e46856c01eaad5ae34ca
youssef <youssefzekry97@gmail.com>
2023-02-22 11:26:29 +0200

    Use @Around

--- src/main/java/com/mypackage/CalcBefore.java
+++ src/main/java/com/mypackage/CalcBefore.java
@@ -6,11 +6,15 @@ import java.util.EmptyStackException;
 import javax.naming.spi.DirStateFactory.Result;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 @Aspect
 public class CalcBefore {
@@ -52,6 +56,23 @@ public class CalcBefore {
 					+ "\n(afterThrowingAdd) arguments: "
 					+ Arrays.toString(joinPoint.getArgs())
 					+ "*****************EmptyStackException.....****************");
-		
 			}
+
+		@Around("execution(* com.mypackage..add(..))")
+		public Object AroundAdd(ProceedingJoinPoint joinPoint) 
+			throws Throwable{
+				System.out.println("(AroundAdd)before method: "
+					+ joinPoint.getSignature().getName()
+					+ "\n(AroundAdd)before arguments: ");
+					Object result;
+					try{
+						result = joinPoint.proceed();
+					} catch(Exception ex){
+						ex.printStackTrace();
+						throw ex;
+					}
+					System.out.println("(AroundAdd)after");
+					return result;
+			}
+	
 }
--- src/main/java/com/mypackage/CalcService.java
+++ src/main/java/com/mypackage/CalcService.java
@@ -1,12 +1,10 @@
 package com.mypackage;
 
-import java.util.EmptyStackException;
-
 public class CalcService{
 
 	public double add(double num1, double num2) {
 		double result = num1 + num2;
-		throw new EmptyStackException();
+		return result;
 	}
 
 	public double sub(double num1, double num2) {
