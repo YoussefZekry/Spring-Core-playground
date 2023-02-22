aa12a3c1c30eed63d52bf596c4e4030752402ecf
youssef <youssefzekry97@gmail.com>
2023-02-22 10:07:31 +0200

    Use JoinPoint as argument for "beforeMulti" method

--- src/main/java/com/mypackage/CalcBefore.java
+++ src/main/java/com/mypackage/CalcBefore.java
@@ -1,5 +1,8 @@
 package com.mypackage;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -7,7 +10,16 @@ import org.aspectj.lang.annotation.Before;
 public class CalcBefore {
 	
 	@Before("execution(* com.mypackage..add(..))")
-	public void before() {
-		System.out.println("lalalalalalalalala");
+	public void beforeAdd() {
+		System.out.println("BeforeAdd");
+	}
+	@Before("execution(* com.mypackage..multi(..))")
+	public void beforeMulti(JoinPoint joinPoint) 
+	throws Throwable{
+		System.out.println("(BeforeMulti) method: "
+			+ joinPoint.getSignature().getName()
+			+ "\n(BeforeMulti) arguments: "
+			+ Arrays.toString(joinPoint.getArgs())
+			);
 	}
 }
